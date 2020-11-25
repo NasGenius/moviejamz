d6484a0c5c85c600bc89944e7ba320491fb718b6
Ryan Hubble <rhubble1987@gmail.com>
2020-11-24 21:33:45 -0500

    Updated imdb js file to make the call to itunes to pull the basic album data

--- imdb_call.js
+++ imdb_call.js
@@ -2,7 +2,7 @@ var movie = "cats";
 var movieYear = "2019"
 var imdbSearchURL = "https://imdb-api.com/en/API/SearchMovie/k_7ln5s4c3/" + movie + "" + movieYear;
 
-$.ajax({
+$.ajax({//First ajax call just gets the basic movie info like the official title, the poster, and it's IMDB id
     url: imdbSearchURL,
     method: 'GET'
 }).then(function(response) {
@@ -13,7 +13,7 @@ var movieInfo = {
     id: response.results[0].id
 }
 var imdbFullActorSearchURL = "https://imdb-api.com/en/API/Title/k_7ln5s4c3/" + movieInfo.id + "/FullActor,FullCast";
-$.ajax({
+$.ajax({ //Second ajax call gets additional movie info to be used to add more items to search within itunes
     url: imdbFullActorSearchURL,
     method: "GET"
 }).then(function(response){
@@ -29,6 +29,46 @@ $.ajax({
 $('#movie-poster').attr('src', movieInfo.poster);
 $('.movie-info').append('<ul><li><h3>' + movieInfo.title + '</h3></li><li>Release Year: ' + castAndCrewInfo.year + '</li><li>Music by: ' + castAndCrewInfo.composer + '</li><li>Starring: ' + castAndCrewInfo.stars + '</li><li>Directed by: ' + castAndCrewInfo.director + '</li></ul>')
 
+
+var limitOfSearchResults = 5;
+var soundtrackURL = 'https://itunes.apple.com/search?media=music&term=' + movieInfo.title + '+motion+picture+soundtrack&limit=' + limitOfSearchResults + '&entity=album&attribute=albumTerm';
+//var composerURL = 
+//var actorsURL =
+
+console.log(soundtrackURL);
+
+$.ajax({//third ajax call returns any albums related to the movie along with soundtrack info
+    url: soundtrackURL,
+    method: 'GET'
+}).then(function(response){
+    var formattedResponse = JSON.parse(response);
+    for (i = 0; i < 5; i++) {
+        console.log(formattedResponse.results[i]);
+        var genre = formattedResponse.results[i].primaryGenreName;
+        var albumRelease = formattedResponse.results[i].releaseDate;
+        var albumYear = albumRelease.substr(0,4);
+        console.log(albumYear);
+        if (genre === "Soundtrack" && albumYear == castAndCrewInfo.year) {
+            var soundtrackInfo = {
+            albumName: formattedResponse.results[i].collectionName,
+            albumID: formattedResponse.results[i].collectionId,
+            albumURL: formattedResponse.results[i].collectionViewUrl,
+            };
+            i = 5;
+            console.log(soundtrackInfo);
+
+        }
+    }
+
+    /* if (genre = "Soundtrack") { *
+        var formattedResponse = JSON.parse(response);
+        console.log(formattedResponse);
+        console.log(typeof formattedResponse);
+/*    } else */
+    
+    //var soundtrackInfo = {
+
+    });
 
 });
 
