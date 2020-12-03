1de4c2b33b3ede6f1ccad203ac1a56a32d30288b
Austyn Whaley <70417091+austynwhaley@users.noreply.github.com>
2020-12-03 15:29:00 -0500

    new local storage feature added and mobile nav burger added

--- imdb_call.js
+++ imdb_call.js
@@ -1,18 +1,9 @@
+var currentYear = moment().format('YYYY');
+for (i = 1895; i <= currentYear; i++) {
+    $('#movie-year').append('<option>' + i + '</option>');
+}
 
-    var currentYear = moment().format('YYYY');
-    console.log(currentYear);
-    for (i = 1895; i <= currentYear; i++) {
-        $('#movie-year').append('<option>' + i + '</option>');
-    }
-
-
-
-/* searchBar[0].addEventListener('keyup', function(searchString) {
-    return searchBar = searchString.target.value;
-      
-});  */
-
-
+// function for imdb api call
 function movieSearch() {
 
 $('#soundtrack').empty();    
@@ -21,9 +12,9 @@ var movie = $('#searchBar').val();
 var movieYear = $('select').val();
 
 if (movieYear == 'Select Year (optional)') {
-    var movieSearchURL = "https://imdb-api.com/en/API/SearchMovie/k_7ln5s4c3/" + encodeURIComponent(movie);
+    var movieSearchURL = "https://imdb-api.com/en/API/SearchMovie/k_pfo2aetw/" + encodeURIComponent(movie);
 } else {
-    var movieSearchURL = "https://imdb-api.com/en/API/SearchMovie/k_7ln5s4c3/" + encodeURIComponent(movie) + '%20' + movieYear;
+    var movieSearchURL = "https://imdb-api.com/en/API/SearchMovie/k_pfo2aetw/" + encodeURIComponent(movie) + '%20' + movieYear;
 };
     
 console.log(movieSearchURL);
@@ -40,7 +31,7 @@ console.log(movieSearchURL);
         poster: response.results[0].image,
         id: response.results[0].id
     }
-    var imdbFullActorSearchURL = "https://imdb-api.com/en/API/Title/k_7ln5s4c3/" + movieInfo.id + "/FullActor,FullCast";
+    var imdbFullActorSearchURL = "https://imdb-api.com/en/API/Title/k_pfo2aetw/" + movieInfo.id + "/FullActor,FullCast";
     $.ajax({
         url: imdbFullActorSearchURL,
         method: "GET"
@@ -59,20 +50,17 @@ console.log(movieSearchURL);
         $('#composer').text(castAndCrewInfo.composer);
         $('#starring').text(castAndCrewInfo.stars);
         $('#director').text(castAndCrewInfo.director);
-        //$('#movie-info').append('<div class="notification is-dark"><p><b>Composer:</b> ' + castAndCrewInfo.composer + '</p></div><div class="notification is-dark"><p><b>Starring:</b> ' + castAndCrewInfo.stars + '</p></div><div class="notification is-dark"><p><b>Director:</b> ' + castAndCrewInfo.director + '</p></div>' )
-/*         $('#movie-stars').text("Cast: " + castAndCrewInfo.stars);
-        $('#movie-directors').text("Director: " + castAndCrewInfo.director); */
-        //$('.movie-info').append('<ul><li><h3>' + movieInfo.title + '</h3></li><li>Release Year: ' + castAndCrewInfo.year + '</li><li>Music by: ' + castAndCrewInfo.composer + '</li><li>Starring: ' + castAndCrewInfo.stars + '</li><li>Directed by: ' + castAndCrewInfo.director + '</li></ul>');
+    
 
 //Need to remove special characters from the title returned from imdb to prevent issues with the itunes search
-var limitOfSearchResults = 5;
+
 var movieTitleWithoutColon = movieInfo.title.replace(/:/g,"");
 var movieTitleWithoutHyphen = movieTitleWithoutColon.replace(/-/g,"");
 var movieTitleEncoded = encodeURIComponent(movieTitleWithoutHyphen);
 var movieTitleWithPlus = movieTitleEncoded.replace(/%20/g,"+");
 console.log(movieTitleEncoded);
 console.log(movieTitleWithPlus);
-var soundtrackURL = 'https://itunes.apple.com/search?media=music&term=' + movieTitleWithPlus + '+motion+picture+soundtrack&limit=' + limitOfSearchResults + '&entity=album&attribute=albumTerm';
+var soundtrackURL = 'https://itunes.apple.com/search?term=' + movieTitleWithPlus + '+soundtrack';
  
 console.log(soundtrackURL);
 
@@ -84,9 +72,9 @@ $.ajax({ //third ajax call returns any albums related to the movie along with so
     var formattedResponse = JSON.parse(response);
     if (formattedResponse.resultCount == 0) {
         console.log("No results found");
-        $('#soundtrack').append('<div class="notification is-dark"><p class="is-size-4">No soundtrack could be found.</p></div>')
+        $('#soundtrack').append('<div class="notification is-dark"><p class="is-size-4">No soundtrack could be found.</p></div>');
     } else {
-    for (i = 0; i < 5; i++) {
+    for (i = 0; i < formattedResponse.resultCount; i++) {
         console.log(formattedResponse.results[i]);
         var genre = formattedResponse.results[i].primaryGenreName;
         var albumRelease = formattedResponse.results[i].releaseDate;
@@ -98,7 +86,7 @@ $.ajax({ //third ajax call returns any albums related to the movie along with so
             albumID: formattedResponse.results[i].collectionId,
             albumURL: formattedResponse.results[i].collectionViewUrl,
             };
-            i = 5;
+            i = formattedResponse.resultCount;
             console.log(soundtrackInfo);
             var tracklistURL = 'https://itunes.apple.com/lookup?id=' + soundtrackInfo.albumID + '&entity=song'
             $.ajax({
@@ -115,32 +103,72 @@ $.ajax({ //third ajax call returns any albums related to the movie along with so
                         trackURL: formattedTracklistResponse.results[i].trackViewUrl
                     };
                     console.log(track);
-                    $('#tracklist').append('<li><a href="' + track.trackURL + '" target="_blank">' + track.trackName + '</a></li>')
+                    $('#tracklist').append('<li class="track" ><p><a href="' + track.trackURL + '" target="_blank">' + track.trackName + '</a></p></li>')
+                
+                
                 }
             })
 
 
-        }
+        } 
+
     }
 }
 
-    });
+
+});
 
 });
     }
 
 });
 
-
-
-
 }
 
 
+//Event listener to start the search
+var trackList = $('#saved-songs')
+$('#searchBtn').on('click', function() {
 
+    //stores users input and appends to search history
+    var key = 'movietitle';
+    var value = $('#searchBar').val();
 
+    if (key && value) {
+        localStorage.setItem(key, value)
+        
+    }
 
-$('#searchBtn').on('click', function(event) {
-    event.preventDefault();
+    for (var i = 0; i <localStorage.length; i++) {
+        const key = localStorage.key(i);
+        const value = localStorage.getItem(key)
+    
+        trackList.append(`<a href=""> ${value} </a>`) 
+    }
+    
     movieSearch();
 });
+
+for (var i = 0; i <localStorage.length; i++) {
+    const key = localStorage.key(i);
+    const value = localStorage.getItem(key)
+
+    trackList.append(`<a href=""> ${value} </a>`) 
+}
+
+
+
+//Code to save and store songs
+
+
+// function saveSong(event) {
+//     event.preventDefault()
+//     var savedSongs = $('#saved-songs');
+//     savedSongs.append($('#track-name').text());
+//     localStorage.setItem = ('savedSongs', JSON.stringify(savedSongs));
+// }
+
+// $('document').on('click', {
+//     trackURL: $(this).siblings().attr('href'),
+//     trackName: $(this).siblings().text()
+// }, saveSong);
